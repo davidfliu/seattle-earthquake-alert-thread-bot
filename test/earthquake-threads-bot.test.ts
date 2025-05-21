b3c6d38f5e9f6949452d263be299327bd2f05617
David Liu <19348355+davidfliu@users.noreply.github.com>
2025-05-21 00:14:28 -0700

    feat: setup basic CDK project structure

---
+++ test/earthquake-threads-bot.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EarthquakeThreadsBot from '../lib/earthquake-threads-bot-stack';
+
+// example test. To run these tests, uncomment the lines below
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EarthquakeThreadsBot.EarthquakeThreadsBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
